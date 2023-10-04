542f6a27012993be5dfd9c03135a23e033a21d98
Michael Ripley <zkxs00@gmail.com>
2023-10-04 12:12:15 -0500

    fix project setup

---
+++ NeosModTest/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NeosModTest")]
+[assembly: AssemblyProduct("NeosModTest")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
+[assembly: AssemblyDescription("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
